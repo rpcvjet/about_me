3f212844a9486b3d23803b5fd701208879cc8779
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2016-09-21 14:19:38 -0700

    lab re-submit with console messages.

--- app.js
+++ app.js
@@ -2,34 +2,65 @@
 
 var counter = 0;
 
-prompt('Let\'s Play a Game! Choose Yes or No. Press enter when you are ready to begin');
+prompt('Let\'s Play a Game!  Click OK when you are ready to begin');
 var spicy = prompt('Does Kenneth like spicy food?');
 if (spicy.toUpperCase() === 'YES' || spicy.toUpperCase() === 'Y')
-{counter++;
-  console.log (spicy.toUpperCase());}
-else{};
+
+{alert('That\'s right!');
+  console.log ('I love spicy food!');
+  counter++;
+}
+else {
+  alert('Sorry, that is incorrect');
+  console.log ('I love spicy food!');
+};
 
 var state = prompt ('Was Kenneth born in Alabama?');
 
 if (state.toUpperCase() === 'NO' || state.toUpperCase() === 'N')
-{counter++;}
+{alert('Correct. I was born in Florida');
+  console.log('I was born in Florida');
+  counter++;
+}
 
-else{};
+else{
+  alert('Uhh, no.');
+  console.log ('I was born in Florida');
+};
 
 var sport = prompt ('Did Kenneth play football in college?');
 if (sport.toUpperCase() === 'NO' || sport.toUpperCase() === 'N')
-{counter++;}
-else{};
+{alert('Correct. I did not play football. I was on the crew team.');
+  console.log('I did not play football. I was on the rowing team.');
+  counter++;}
+else{
+  alert('WRONG! I\'m no meathead.');
+  console.log ('I did not play football. I was on the rowing team.');
+};
 
 var dreamjob = prompt ('Will Kenneth ever be an employed software engineer?');
 if (dreamjob.toUpperCase() === 'YES' || dreamjob.toUpperCase() === 'Y')
-{counter++;}
-else{}
+{alert('Correct. I cetainly hope so at least.');
+  console.log('Yes he will become a software engineer.');
+  counter++;
+}
+
+else{
+  alert('Incorrect! Have you no faith in me?');
+  console.log ('Yes he will become a software engineer.');
+}
 
 var handsome = prompt ('Is Kenneth the most handsome man in Code 201?');
 if (handsome.toUpperCase() === 'YES' || handsome.toUpperCase() === 'Y')
-{counter++;}
-else{};
+{alert('Correct. Well, obviously!');
+  console.log('I\'m too sexy for this course');
+  counter++;
+}
+
+else{
+  alert('Incorrect! Look around, truly I am the best goodlooking');
+  console.log ('Kenneth is the best looking man.');
+};
 
 if (counter === 5)
    {prompt('Congratulations. You got all ' + counter + ' questions correct. You are the champion!');}
